e8c8591e4b29e36ca354e6e77c02e53ee3dbfbfe
Marianela Zinni <marianela.zinni@gmail.com>
2025-10-18 21:10:37 -0300

    creacion de migraciones, seeders y conexion a la bs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            SubjectTypeSeeder::class,
+            SubjectSeeder::class,
+            StudentSeeder::class
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
