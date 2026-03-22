326ce1b6bf4fd40372b6b8d04d86351c6b33a963
Marianela Zinni <marianela.zinni@gmail.com>
2026-03-22 01:16:14 -0300

    no ir a una pagina en blanco al hacer clic en ir al alumno desde la lista de asistencia

--- resources/views/attendance/take.blade.php
+++ resources/views/attendance/take.blade.php
@@ -102,7 +102,7 @@
                            <a href="{{ route('students.edit', $student) }}"
                                title="Editar ficha de {{ $student->name }}"
                                class="inline-flex items-center justify-center h-7 w-7 rounded-full text-[#29b1dc] hover:bg-[#29b1dc]/10 transition"
-                               target="_blank">
+                               >
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"/>
